ef69ba6d00a869a67291fe613d0a2cf31637c7a5
Alex Pescaru <apescaru11@gmail.com>
2022-01-02 17:32:45 +0200

    initial commit

---
+++ MiniJava/src/main/MainClass.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.io.*;
+import mainframe.MainFrame;
+import minijavaparser.*;
+
+public class MainClass 
+{
+	public static void main(String args[]) 
+	{
+	    try 
+	    {
+	      	MiniJava p = new MiniJava(new FileInputStream(new File("./samples/test01.java")));
+	    	ASTProgram root = p.Program();
+	    	//root.dump(">");
+
+			MainFrame frame=new MainFrame(root);
+			frame.setVisible(true);			
+			
+			System.out.println("Thank you.");
+	    } 
+	    catch(Exception e)
+	    {									      
+			System.err.println("Oops.");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+	    }
+	}
+}
